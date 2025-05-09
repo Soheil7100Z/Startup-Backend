b28fe3bd966668cd23c7ecd8e315e500d7c3d772
Soheil Zaremehrjardi <soheil7100@gmail.com>
2025-05-09 20:27:09 +0200

    Startup website Project => added item page for single product and get requests at backend to get all products and single product using server on port 3000

--- StartUp-Backend.js
+++ StartUp-Backend.js
@@ -3,22 +3,36 @@ const {engine} = require('express-handlebars')
 const nodemailer = require('nodemailer')
 const cors = require('cors');
 const PORT = 3000
+const products = require('./products.json');
 
-const emailingApp = express()
+const App = express()
 
-emailingApp.engine('handlebars' , engine())
-emailingApp.set('view engine' , 'handlebars')
+App.engine('handlebars' , engine())
+App.set('view engine' , 'handlebars')
 
-
-emailingApp.use(cors({
+App.use(cors({
     origin: 'http://localhost:5000',
     methods: ['GET', 'POST'],
     credentials: true
   }));
-emailingApp.use(express.json())
+App.use(express.json())
 
+App.get('/products', (req, res) => {
+  res.json(products);
+});
 
-emailingApp.post('/send' ,(req,res) =>{
+App.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  const product = products.find(p => p.id == id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).send('Not Found');
+  }
+});
+
+App.post('/send' ,(req,res) =>{
    const output = `
    <p style="font-size: 1.2rem; font-weight: bold; color: black" >Besucher*in hat Ihr Kontaktformular ausgefüllt:</p>
    <ul>
@@ -35,8 +49,8 @@ emailingApp.post('/send' ,(req,res) =>{
     port: 465,
     secure: true,
     auth: {
-      user: "", // give your gmail
-      pass: "", //  give app password of your gmail
+      user: "000000@gmail.com", // give your gmail
+      pass: "*********", //  give app password of your gmail
     },
   });
 
@@ -44,8 +58,8 @@ emailingApp.post('/send' ,(req,res) =>{
   async function FromTo() {
 
     const emailInfo = await emailTransporter.sendMail({
-      from: '"Soheil Zaremehrjardi" <soheilpro13max@gmail.com>',
-      to: "soheil7100@gmail.com",
+      from: '"your name" <000000@gmail.com>', //give your gmail
+      to: "111111@gmail.com", //give your gmail you want to send to
       subject: "Kontaktformular - StartUp Project",
       text: "",
       html: output,
@@ -59,4 +73,4 @@ emailingApp.post('/send' ,(req,res) =>{
   FromTo().catch(console.error);
 })
 
-emailingApp.listen(PORT ,() => console.log('server is working') )
+App.listen(PORT ,() => console.log('server is working') )
